7509df56af0ce3457f2d22fc316637b4a0f15804
Renan Souza <renantrsouza@gmail.com>
2021-10-12 12:35:03 -0400

    used enviroments

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,19 +1,19 @@
-import firebase from "firebase/app";
+import { initializeApp } from "firebase/app";
 
-import 'firebase/auth';
-import 'firebase/database';
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBM72YDPdV0Mdn_SOQUC0rZepAIjHugFmc",
-    authDomain: "letmeask-6bc71.firebaseapp.com",
-    databaseURL: "https://letmeask-6bc71-default-rtdb.firebaseio.com",
-    projectId: "letmeask-6bc71",
-    storageBucket: "letmeask-6bc71.appspot.com",
-    messagingSenderId: "723020375855",
-    appId: "1:723020375855:web:d3c5131b13561c40e6e3da"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATA_BASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
   };
 
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
-export const auth = firebase.auth();
-export const database = firebase.database();
+export const auth = getAuth();
+export const database = getDatabase();
