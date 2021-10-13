b1f8ea9a63c730f4e364d0a61e96765a36e6eff0
Renan Souza <renantrsouza@gmail.com>
2021-10-13 11:15:06 -0400

    add main

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,6 @@
 import illustrationSvg from "../assets/images/illustration.svg";
+import logoImg from "../assets/images/logo.svg";
+import googleIconImg from "../assets/images/google-icon.svg";
 
 export function Home(){
     return (
@@ -9,7 +11,23 @@ export function Home(){
                 <p>Tire as dúvidas da sua audiência em tempo real.</p>
             </aside>
             <main>
-
+                <div>
+                    <img src={logoImg} alt="Letmeask" />
+                    <button>
+                        <img src={googleIconImg} alt="Logo do Google" />
+                        Crie sua sala com o Google
+                    </button>
+                    <div>ou entre em uma sala</div>
+                    <form>
+                        <input
+                            type="text"
+                            placeholder="Digite o código da sala"
+                        />
+                        <button type="submit">
+                            Entrar na sala
+                        </button>
+                    </form>
+                </div>
             </main>
         </div>
     );
