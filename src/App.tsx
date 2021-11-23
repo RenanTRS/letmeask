9a73ddb03515ea4d2ed5acd573774ca2744c0ab7
Renan Souza <renantrsouza@gmail.com>
2021-11-23 13:33:22 -0400

    add unsubscribe

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
   const [user, setUser] = useState<User>();
 
   useEffect(()=>{
-    auth.onAuthStateChanged(user=>{
+    const unsubscribe = auth.onAuthStateChanged(user=>{
       if(user){
         const {displayName, photoURL, uid} = user;
   
@@ -42,6 +42,10 @@ function App() {
         });
       }
     });
+
+    return () => {
+      unsubscribe();
+    }
   }, []);
 
   async function signInWithGoogle(){
