9b884d7198d10a2822911b3f1d8de7bd976a49af
Renan Souza <renantrsouza@gmail.com>
2021-11-29 14:20:57 -0400

    Crete Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { AuthContextProvider } from './contexts/AuthContexts'
 
 /*Pages*/
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
+import { Room } from './pages/Room'
 
 /*Styles*/
 import "./styles/global.scss";
@@ -13,8 +14,11 @@ function App() {
   return (
     <BrowserRouter>
       <AuthContextProvider>
+        <Switch>
           <Route path="/" exact component={Home}/>
           <Route path="/rooms/new" component={NewRoom}/>
+          <Route path="/rooms/:id" component={Room} />
+        </Switch>
       </AuthContextProvider>
     </BrowserRouter>
   );
