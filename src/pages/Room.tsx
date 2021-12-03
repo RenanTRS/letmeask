4a01b48c3cd7fbf732965cdb51124c4942aee7cd
Renan Souza <renantrsouza@gmail.com>
2021-12-03 14:11:21 -0400

    save question within realtime database

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -5,7 +5,12 @@ import { RoomCode } from '../Components/RoomCode';
 
 import { useParams } from 'react-router-dom';
 
+import { FormEvent, useState } from 'react';
+
 import '../styles/room.scss';
+import { useAuth } from '../hooks/useAuth';
+
+import {database} from '../services/firebase';
 
 
 type RoomParams = {
@@ -13,8 +18,44 @@ type RoomParams = {
 }
 
 export function Room(){
+    //Pega o parâmetro id da rota
     const params = useParams<RoomParams>();
     const roomId = params.id;
+    
+    //Pega o objeto user do hook
+    const {user} = useAuth();
+
+    const [newQuestion, setNewQuestion] = useState('');
+
+    async function handleSendQuestion(event: FormEvent){
+        event.preventDefault();
+
+        //Se caso for vazio
+        if(newQuestion.trim() === ''){
+            return;
+        }
+        //Se caso o usuário não for autenticado
+        if(!user){
+            throw new Error('You must be logged in.');
+        }
+
+        //Objeto contendo dados da pergunta
+        const question = {
+            content: newQuestion, //pergunta
+            author: {
+                name: user.name, //nome do usuário
+                avatar: user.avatar //foto do usuáior
+            },
+            isHighLighted: false, //se está sendo respondida neste momento
+            isAnswered: false //se já foi respondida
+        }
+
+        //Salva no banco, pelo caminho rooms/o id da sala/pergunta
+        await database.ref(`rooms/${roomId}/questions`).push(question);
+
+        //Limpa o textarea
+        setNewQuestion('');
+    }
 
 
     return (
@@ -32,12 +73,17 @@ export function Room(){
                     <span>4 perguntas</span>
                 </div>
 
-                <form>
-                    <textarea placeholder='O que você quer perguntar?'></textarea>
+                <form onSubmit={handleSendQuestion}>
+                    <textarea 
+                        placeholder='O que você quer perguntar?'
+                        onChange={event => setNewQuestion(event.target.value)}
+                        value={newQuestion}
+                    />
 
                     <div className='form-footer'>
                         <span>Para fazer uma pergunta, <button>faça seu login</button>.</span>
-                        <Button type="submit">Enviar pergunta</Button>
+                        {/*Botão fica disbled caso não tenha usuário*/}
+                        <Button type="submit" disabled={!user}>Enviar pergunta</Button>
                     </div>
                 </form>
             </main>
