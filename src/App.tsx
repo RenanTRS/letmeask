31a96e5da675018dee4f3b17148b8ada0ad68a6f
Renan Souza <renantrsouza@gmail.com>
2022-02-25 15:36:05 -0400

    create route for admin

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { AuthContextProvider } from './contexts/AuthContexts'
+import { AdminRoom } from "./pages/AdminRoom";
 
 /*Pages*/
 import { Home } from "./pages/Home";
@@ -18,6 +19,7 @@ function App() {
           <Route path="/" exact component={Home}/>
           <Route path="/rooms/new" component={NewRoom}/>
           <Route path="/rooms/:id" component={Room} />
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
