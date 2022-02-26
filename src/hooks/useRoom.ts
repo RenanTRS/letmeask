dd6322ffabb60173ac0cf527fc4ec07a5f81ce24
Renan Souza <renantrsouza@gmail.com>
2022-02-26 14:51:59 -0400

    adiciona e remove likes

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -1,5 +1,5 @@
 import {useState, useEffect} from 'react'
-import { isObjectLiteralElement } from 'typescript';
+//import { isObjectLiteralElement } from 'typescript';
 import { database } from '../services/firebase';
 import { useAuth } from './useAuth';
 
@@ -27,7 +27,7 @@ type QuestionType = {
     isAnswered: boolean;
     isHighlighted: boolean;
     likeCount: number;
-    hasLiked: boolean;
+    likeId: string | undefined;
 }
 
 
@@ -52,7 +52,7 @@ export function useRoom(roomId: string){
                     isHighlighted: value.isHighlighted,
                     isAnswered: value.isAnswered,
                     likeCount: Object.values(value.likes ?? {}).length,
-                    hasLiked: Object.values(value.likes ?? {}).some(like => like.authorId === user?.id)
+                    likeId: Object.entries(value.likes ?? {}).find(([key, like]) => like.authorId === user?.id)?.[0]
                 }
             });
             //console.log(parseQuestion);
